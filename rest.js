b9689e5a82bb89c649980c2acd3e6d2b5bec879d
helmet33 <garrettjor@gmail.com>
2015-09-28 13:48:07 +0100

    Added express
    Express

--- rest.js
+++ rest.js
@@ -9,11 +9,15 @@ var todos = [
 ];
 
 app.get('/', function(req, res) {
-    res.json({ message: 'It works!' });   
+    res.json({ message: 'It works!' });
 });
 
 app.get('/todo', function(req, res) {
-    res.json(todos);   
+    res.json(todos);
+});
+
+app.get('/todo/:id', function(req, res) {
+    res.json(todos[req.params.id]);
 });
 
 var server = app.listen(8000);
